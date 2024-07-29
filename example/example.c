6bb52a78d2246b90e3378ff973d8b05e192346ef
voidxptr <void.o.ptr@gmail.com>
2024-07-29 15:06:15 +0200

    Moved header from include to base dir.

--- example/example.c
+++ example/example.c
@@ -1,4 +1,4 @@
-#include "../include/suclog.h"
+#include "../suclog.h"
 
 int main(int argc, char* argv[])
 {
--- src/logger.c
+++ src/logger.c
@@ -1,4 +1,4 @@
-#include "../include/suclog.h"
+#include "../suclog.h"
 #include <stdarg.h>
 #include <stdio.h>
 #include <time.h>
